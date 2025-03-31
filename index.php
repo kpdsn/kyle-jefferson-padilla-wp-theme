8612174c37b9f2e119462d8d8d4a9debbaa74a5c
kpdsn <2365822@johnabbottcollege.net>
2025-03-31 11:55:12 -0400

    reach till step 2.9

--- index.php
+++ index.php
@@ -12,3 +12,43 @@
 
     <!--  END WORDPRESS GET_HEADER CONTENT IN INDEX.PHP -->
     <!-- STEP 2.3 ENDS HERE -->
+
+        <!-- START STEP 2.6 ENABLE FOOTER -->
+
+
+        <?php get_footer(); ?>
+        <?php // this functions loads the footer.php file ?>   
+
+
+    <!-- END STEP 2.6 ENABLE FOOTER -->
+
+
+      <!-- START STEP 2.8 ADD H1 AND WORDPRESS LOOP -->
+  <h1><?php the_title(); // AUTOMATICALLY ADDS THE PAGE TITLE ?></h1>
+  
+  <!-- opening flex-container will go here -->
+  
+  <main id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+  <?php // NOTE IDs AND CLASSES ARE AUTOMATICALLY ADDED HERE ?>
+
+
+  <!-- START WORDPRESS LOOP -->
+
+      <?php 
+          if ( have_posts() ) : 
+          while ( have_posts() ) : the_post();
+              the_content();
+          endwhile;
+      else :
+          _e( 'Sorry, no posts matched your criteria.', 'kyle-padilla' );
+          // MAKE SURE TO CHANGE BILLY-POPPINS HERE
+      endif;
+      ?>
+
+      <?php wp_link_pages(); ?>
+
+  <!-- END WORDPRESS LOOP -->    
+  </main>
+
+   <!-- END STEP 2.8 ADD H1 AND WORDPRESS LOOP -->
+  <!-- closing flex-container will go here -->
